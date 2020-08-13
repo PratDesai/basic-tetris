8d62c6603436a08b98d11ef0ebbcd0c8296eedb2
Pratik Desai <pdesai@ricelake.com>
2020-08-12 21:56:45 -0500

    Added display window for the next tetromino. Added score tracking without levels for now.

--- app.js
+++ app.js
@@ -2,72 +2,119 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   const width = 10;
   const ScoreDisplay = document.querySelector("#score");
+  console.log(ScoreDisplay);
   const StartPauseButton = document.querySelector("#start-pause-button");
-  var squares = document.querySelectorAll(".grid div");
   var squareArray = Array.from(document.querySelectorAll(".grid div"));
+  var displayArray = Array.from(document.querySelectorAll(".mini-grid div"));
 
-  // the arrayItem
-  const lTetromino = [
-    [1, 2, width + 1, 2 * width + 1],
-    [width, width + 1, width + 2, 2 * width + 2],
-    [1, width + 1, 2 * width, 2 * width + 1],
-    [width, 2 * width, 2 * width + 1, 2 * width + 2],
-  ];
-  const zTetromino = [
-    [width + 1, width + 2, 2 * width, 2 * width + 1],
-    [0, width, width + 1, 2 * width + 1],
-    [width + 1, width + 2, 2 * width, 2 * width + 1],
-    [0, width, width + 1, 2 * width + 1],
-  ];
-  const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, 2 * width + 1],
-    [width, width + 1, width + 2, 2 * width + 1],
-    [1, width, width + 1, 2 * width + 1],
-  ];
-  const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-  ];
-  const iTetromino = [
-    [1, width + 1, 2 * width + 1, 3 * width + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, 2 * width + 1, 3 * width + 1],
-    [width, width + 1, width + 2, width + 3],
-  ];
+  const createTetrominoes = function (width) {
+    let tetrominoes = [
+      [
+        [1, 2, width + 1, 2 * width + 1],
+        [width, width + 1, width + 2, 2 * width + 2],
+        [1, width + 1, 2 * width, 2 * width + 1],
+        [width, 2 * width, 2 * width + 1, 2 * width + 2],
+      ],
+      [
+        [width + 1, width + 2, 2 * width, 2 * width + 1],
+        [0, width, width + 1, 2 * width + 1],
+        [width + 1, width + 2, 2 * width, 2 * width + 1],
+        [0, width, width + 1, 2 * width + 1],
+      ],
+      [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, 2 * width + 1],
+        [width, width + 1, width + 2, 2 * width + 1],
+        [1, width, width + 1, 2 * width + 1],
+      ],
+      [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+      ],
+      [
+        [1, width + 1, 2 * width + 1, 3 * width + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, 2 * width + 1, 3 * width + 1],
+        [width, width + 1, width + 2, width + 3],
+      ],
+    ];
+    return tetrominoes;
+  };
 
-  const theTetrominoes = [
-    lTetromino,
-    zTetromino,
-    tTetromino,
-    oTetromino,
-    iTetromino,
-  ];
+  const theTetrominoes = createTetrominoes(width);
+  const displayTetrominoes = createTetrominoes(4);
 
-  const getRandomItem = function (arrayItem) {
-    let randomIndex = Math.floor(Math.random() * arrayItem.length);
-    return arrayItem[randomIndex];
+  const getRandomTetrominoIndex = function () {
+    return Math.floor(Math.random() * theTetrominoes.length);
   };
 
   // app state variables
   let currentPosition = 4;
   let currentRotation = 0;
-  let currentTetromino = getRandomItem(theTetrominoes);
-  let currentTetroinoRotation = currentTetromino[currentRotation];
+  let nextTetrominosIndex = getRandomTetrominoIndex();
+  let currentTetromino;
+  let currentTetroinoRotation;
+  let displayTetroinoRotation;
   let timerId;
+  let score = 0;
+  let level = 0;
+
+  const getPoints = function (linesCleared, level) {
+    switch (linesCleared) {
+      case 0:
+        return 0;
+      case 1:
+        return 40 * (level + 1);
+      case 2:
+        return 100 * (level + 1);
+      case 3:
+        return 300 * (level + 1);
+      case 4:
+        return 1200 * (level + 1);
+      default:
+        return 1200 * (level + 1);
+    }
+  };
+
+  const UpdateScoreDisplay = function (linesCleared) {
+    var points = getPoints(linesCleared, level);
+    score += points;
+    ScoreDisplay.textContent = score;
+  };
+
+  const updateTetromino = function () {
+    currentTetromino = theTetrominoes[nextTetrominosIndex];
+    currentTetroinoRotation = currentTetromino[currentRotation];
+    nextTetrominosIndex = getRandomTetrominoIndex();
+    displayTetroinoRotation = displayTetrominoes[nextTetrominosIndex][0];
+  };
 
   const drawCurrentRotation = function () {
     clearCurrentRotation();
+    ScoreDisplay;
     currentTetroinoRotation.forEach((index) => {
       squareArray[currentPosition + index].classList.add("tetromino");
+    });
+  };
+
+  const drawDisplayRotation = function () {
+    clearDisplayRotation();
+    displayTetroinoRotation.forEach((index) => {
+      displayArray[index].classList.add("tetromino");
     });
   };
 
   const clearCurrentRotation = function () {
     currentTetroinoRotation.forEach((index) => {
       squareArray[currentPosition + index].classList.remove("tetromino");
+    });
+  };
+
+  const clearDisplayRotation = function () {
+    displayTetroinoRotation.forEach((index) => {
+      displayArray[index].classList.remove("tetromino");
     });
   };
 
@@ -113,39 +160,25 @@ document.addEventListener("DOMContentLoaded", () => {
     drawCurrentRotation();
   };
 
-  let spliceTestData = ["Mars", "Saturn", "Earth", "Pluto"];
-  const testSplice = function () {
-    console.log(spliceTestData);
-    let removedItem = spliceTestData.splice(3, 1);
-    console.log(removedItem);
-    console.log(spliceTestData);
-    spliceTestData = removedItem.concat(spliceTestData);
-    console.log(spliceTestData);
-  };
-
   const removeRow = function (rowIndex) {
     let removedRow = squareArray.splice(width * rowIndex, width);
     removedRow.forEach((x) => x.classList.remove("taken", "tetromino"));
-    console.log(removedRow);
-    console.log(squareArray);
     squareArray = removedRow.concat(squareArray);
     squareArray.forEach((item) => grid.appendChild(item));
-    console.log(squareArray);
   };
 
   const checkRemoveRows = function () {
     let rowIndexes = [...Array(20).keys()];
+    let linesCleared = 0;
     console.log(rowIndexes);
     rowIndexes.forEach((rowIndex) => {
       let row = squareArray.slice(width * rowIndex, width * (rowIndex + 1));
       if (row.every((divItem) => divItem.classList.contains("taken"))) {
         removeRow(rowIndex);
+        linesCleared++;
       }
     });
-    // let lastRow = squareArray.slice(width * 19, width * 20);
-    // if(lastRow.every(x => x.classList.contains('taken'))){
-    //   let removedRow = squareArray.splice(width * 19, width)
-    // }
+    UpdateScoreDisplay(linesCleared, level)
   };
 
   const moveDown = function () {
@@ -156,24 +189,27 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition -= width;
       drawCurrentRotation();
       currentTetroinoRotation.forEach((index) => setTaken(index));
-      currentTetromino = getRandomItem(theTetrominoes);
-      currentTetroinoRotation = currentTetromino[currentRotation];
+      clearDisplayRotation();
+      updateTetromino();
+      drawDisplayRotation();
       setStartPosition();
       checkRemoveRows();
     }
     drawCurrentRotation();
   };
 
+  const canRotateTetromino = function () {
+    let collision = currentTetroinoRotation.some((index) => isTaken(index));
+    let isLeftEdge = currentTetroinoRotation.some((index) => isLeft(index));
+    let isRightEdge = currentTetroinoRotation.some((index) => isRight(index));
+    return !(collision || (isLeftEdge && isRightEdge));
+  };
+
   const rotateTetromino = function () {
     clearCurrentRotation();
     currentRotation = (currentRotation + 1) % currentTetroinoRotation.length;
     currentTetroinoRotation = currentTetromino[currentRotation];
-    const cannotRotate = currentTetroinoRotation.some((index) =>
-      isTaken(index)
-    );
-    const isLeftEdge = currentTetroinoRotation.some((index) => isLeft(index));
-    const isRightEdge = currentTetroinoRotation.some((index) => isRight(index));
-    if (cannotRotate || (isLeftEdge && isRightEdge)) {
+    if (!canRotateTetromino()) {
       currentRotation =
         (currentRotation + currentTetroinoRotation.length - 1) %
         currentTetroinoRotation.length;
@@ -210,12 +246,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  console.log(StartPauseButton);
   StartPauseButton.addEventListener("click", function () {
     startPauseButtonHandler();
   });
 
+  updateTetromino();
   drawCurrentRotation();
+  drawDisplayRotation();
 });
 
 function showHiAlert(firstName, lastName) {
